7ad1845807c4dac6c1e8ab78eeb8ef4860f159f4
Isabel <belpaiva24@gmail.com>
2023-09-22 16:10:09 -0300

    alterações

---
+++ main/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for main project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from principal.views import IndexView, ReservaCreateView, ReservaDeleteView, ReservasListView, ReservaUpdateView, StandCreateView, StandDeleteView, StandListView, StandUpdateView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',IndexView.as_view(),name='index'),
+    path('reserva/', ReservaCreateView.as_view(),name='reserva_criar'),
+    path('reserva/editar/<int:pk>/', ReservaUpdateView.as_view(), name='reserva_editar'),
+    path('reserva/remover/<int:pk>/', ReservaDeleteView.as_view(),name='reserva_remover'),
+    path('reserva/listar', ReservasListView.as_view(),name='reserva_listar'),
+
+    path('stand/', StandListView.as_view(), name ='stand_listar'),
+    path('stand/criar', StandCreateView.as_view(), name= 'stand_criar' ),
+    path('stand/editar/<int:pk>/', StandUpdateView.as_view(), name='stand_editar'),
+    path('stand/remover/<int:pk>/', StandDeleteView.as_view(),name='stand_remover'),
+] + static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ principal/views.py
@@ -0,0 +1,65 @@
+from .models import Reserva, Stand
+from .forms import ReservaForm, StandForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.messages import views
+
+
+# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = "reserva/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_reservas'] = Reserva.objects.count()
+        context['total_stands'] = Stand.objects.count()
+        return context
+
+class ReservasListView(generic.ListView):
+  model = Reserva
+  template_name = "reserva/reserva.html"
+
+class ReservaCreateView(views.SuccessMessageMixin, generic.CreateView):
+  model = Reserva
+  form_class = ReservaForm
+  template_name = "reserva/form.html"
+  success_url = reverse_lazy("reserva_listar")
+  success_message = "Reserva cadastrada com sucesso!"
+  
+class ReservaDeleteView(views.SuccessMessageMixin, generic.DeleteView):
+  model = Reserva
+  template_name = "reserva/reserva_confirm_delete.html"
+  success_url = reverse_lazy("reserva_listar")
+  success_message = "Reserva removida com sucesso!"
+  
+class ReservaUpdateView(views.SuccessMessageMixin, generic.UpdateView):
+  model = Reserva
+  form_class = ReservaForm
+  template_name = "reserva/form.html"
+  success_url = reverse_lazy("reserva_listar")
+  success_message = "Reserva atualizada com sucesso!"
+
+
+#parte dos stands
+class StandListView(generic.ListView):
+  model = Stand
+  template_name = "reserva/stand/stands.html"
+
+class StandCreateView(views.SuccessMessageMixin, generic.CreateView):
+  model = Stand
+  form_class = StandForm
+  template_name = "reserva/stand/form.html"
+  success_url = reverse_lazy("stand_listar")
+  success_message = "Stand cadastrado com sucesso!"
+  
+class StandDeleteView(views.SuccessMessageMixin, generic.DeleteView):
+  model = Stand
+  success_url = reverse_lazy("stand_listar")
+  success_message = "Stand removido com sucesso!"
+  
+class StandUpdateView(views.SuccessMessageMixin, generic.UpdateView):
+  model = Stand
+  form_class = StandForm
+  template_name = "reserva/stand/form.html"
+  success_url = reverse_lazy("stand_listar")
+  success_message = "Stand atualizado com sucesso!"
